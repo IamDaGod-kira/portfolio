877caef555e7a1f428c64843ef435ce6e5ad4414
IamDaGod <mantusantra591@gmail.com>
2023-10-15 14:36:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="bg-gradient-to-br from-blue-700 to-purple-500">
-      <Header />
+      <Header /><br />
       <Body />
     </div>
   );
