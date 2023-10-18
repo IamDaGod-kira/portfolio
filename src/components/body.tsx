dc86d391f0ff165dfc70cc62103758939182c294
Pritam2958 <mantusantra591@gmail.com>
2023-10-18 15:32:46 +0530

    updated body.tsx

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -16,22 +16,22 @@ export default function Body() {
         <br />
         <hr />
         <br />
-        <blockquote className="p-6 rounded-lg shadow-md bg-transparent">
-          <i className="text-4xl text-gray-100 mb-4 leading-none">"</i>
-          <p className="text-lg text-gray-800">
+        <blockquote className="p-6 rounded-lg shadow-md bg-[#333]">
+          <i className="text-4xl text-gray-600 mb-4 leading-none">"</i>
+          <p className="text-lg text-[#4078c0]">
             It's not what you do, but how you do it.
           </p>
-          <i className="text-4xl text-gray-100 mt-4 leading-none">"</i>
+          <i className="text-4xl text-gray-600 mt-4 leading-none">"</i>
         </blockquote>
         <br />
-        <h3>Why do I do what I do ?</h3>
+        <h3 className="text-xl text-white">Why do I do what I do ?</h3>
         <p>
           There is a reason behind it. Now a days, this skill is a very useful
           one, and one can use this for making many good things. As we have
           already reached the 21st century, the near future would treasure this
           skill..
         </p>
-        <h3>What can I do for you?</h3>
+        <h3 className="text-white text-xl">What can I do for you?</h3>
         <p>
           I can develop a react app for you, or go with simple html css, or even
           get you with ubuntu and hacking etc...
