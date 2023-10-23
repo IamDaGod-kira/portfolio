65973633b615c7af8154b38c47a94eca4491a529
Pritam2958 <mantusantra591@gmail.com>
2023-10-23 10:53:05 +0530

    removed counter

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,6 @@ import GitHubRepoList from './components/ghRepos';
 import './App.css';
 
 function App() {
-  React.useEffect(() => {
-    let stop = false;
-
-    if (!stop) {
-      fetch(
-        'https://discord.com/api/webhooks/1165553207863410698/UVBTUC1nvtyk5OiwyB4YDuOXvLoRx7CLxorunpUdUFYhkKrREtqqRQ3hMzv8Vc8NrmSi',
-        {
-          body: JSON.stringify({
-            content: `Someone visited The MoonDeity's Portfolio...`,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-        },
-      )
-        .then(function (res) {
-          console.log(res);
-        })
-        .catch(function (res) {
-          console.log(res);
-        });
-      stop = true;
-    }
-  });
   return (
     <div className="bg-gradient-to-br from-blue-700 to-purple-500">
       <Header />
