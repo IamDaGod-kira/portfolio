7f36d8874ead633d2ff63c71a7bb2159e3b46610
Pritam2958 <mantusantra591@gmail.com>
2023-11-04 16:47:44 +0530

    added staring animation

---
+++ src/components/certificates.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Certs () {
+  return (
+    <><p className="text-blue-600 bg-white">My Certificates</p></>
+  )
+}
--- src/components/stars.tsx
+++ src/components/stars.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 export default function Stars() {
   const [stars, setStars] = React.useState(0);
+  const [clicked, setClicked] = React.useState(false);
 
   React.useEffect(() => {
     const fetchStars = async () => {
@@ -18,11 +19,18 @@ export default function Stars() {
 
     fetchStars();
   });
+  
+  const handleClick = () => {
+    setClicked(!clicked);
+    setStars(stars + 1)
+  }
+  const cname = clicked ? 'fas fa-star pointer-events-none' : 'far fa-star';
+  
   return (
     <>
-      <a href="https://github.com/iamdagod-kira/portfolio/">
+      <a href="https://github.com/iamdagod-kira/portfolio/" target="_blank">
         <div className="fixed text-center bottom-0 right-0 p-4 m-4 bg-gray-900 text-white rounded-full">
-          <i className="far fa-star"></i> <br />
+          <i onClick={handleClick} className={cname}></i> <br />
           {stars}
         </div>
       </a>
