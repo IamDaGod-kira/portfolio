9935818932f577bd6fc2b13300a1593e2571a6b1
IamDaGod <IamDaGod_MoonDeity@protonmail.com>
2024-03-11 15:08:53 +0000

    updated `src/components/body.tsx` . added a renew button to the quotes section.

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -45,7 +45,7 @@ export default function Body() {
           <p className="text-base md:text-lg text-blue-600">{quote.quote}</p>
           <i className="text-4xl text-gray-600 mt-4 leading-none">"</i>
           <p className="text-sm md:text-base">by {quote.author}</p>
-          <button onClick={handleClick}>Renew Quote</button>
+          <button className='text-right text-sm italic underline overline' onClick={handleClick}>🔁</button>
         </blockquote>
         <br />
         <button
